a7f4a3c29409d05e2a7e0ed708eb1799d5340ee4
Gamerbot1212 <gbot03558@gmail.com>
2025-07-10 21:13:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     BOT_TOKEN = os.environ.get("7818844996:AAFEy9ihAxPVKDTHanYvRb-gXEiOfUYxsCw")
-    API_ID = int(os.environ.get("20831039"))
+    API_ID = int(os.environ.get(20831039))
     API_HASH = os.environ.get("ea20b722f7af827db12fb85f4d55238c")
     AUTH_USER = os.environ.get('7597020624').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
